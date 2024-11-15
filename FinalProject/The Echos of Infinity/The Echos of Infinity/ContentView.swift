e125f5571f250a1b5cbd465c3f3edf76af2871f2
James Wang <123661476+DaBanBa@users.noreply.github.com>
2024-11-15 04:17:34 -0500

    FinalProjectCreated

---
+++ FinalProject/The Echos of Infinity/The Echos of Infinity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Echos of Infinity
+//
+//  Created by James Wang on 11/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalProject/The Echos of Infinity/The Echos of Infinity/The_Echos_of_InfinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Echos_of_InfinityApp.swift
+//  The Echos of Infinity
+//
+//  Created by James Wang on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Echos_of_InfinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
